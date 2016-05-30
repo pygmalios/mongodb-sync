81158c5d9ff76f6a8938b0b5a3d7d5000b722a5c
Jan Antala <jan@antala.sk>
2016-05-30 09:23:28 +0200

    Fix mongorestore host

--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p bak
+mkdir -p mongodb_backup
 
 echo "Backing up $MONGO_DATABASE"
-mongodump -h $MONGO_ORIGIN_HOST -d $MONGO_DATABASE -u $MONGO_ORIGIN_USER -p $MONGO_ORIGIN_PASSWORD -o mongodb_backup
+mongodump -h "$MONGO_ORIGIN_HOST" -d "$MONGO_DATABASE" -u "$MONGO_ORIGIN_USER" -p "$MONGO_ORIGIN_PASSWORD" -o mongodb_backup
 
 echo "Uploading backup"
-mongorestore --drop $MONGO_DESTINATION_HOST -d $MONGO_DATABASE -u $MONGO_DESTINATION_USER -p $MONGO_DESTINATION_PASSWORD db_backup/$MONGO_DATABASE
+mongorestore --drop -h "$MONGO_DESTINATION_HOST" -d "$MONGO_DATABASE" -u "$MONGO_DESTINATION_USER" -p "$MONGO_DESTINATION_PASSWORD" "mongodb_backup/$MONGO_DATABASE"
 
 echo "Removing local backup"
 rm -rf mongodb_backup
