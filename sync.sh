80c9bc0af70513627649962bd764062bcbe6cc7c
Jan Antala <jan@antala.sk>
2017-01-08 13:06:45 +0100

    Add MONGO_DESTINATION_AUTH_DB

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ echo "Backing up $MONGO_DATABASE"
 mongodump -h "$MONGO_ORIGIN_HOST" -d "$MONGO_DATABASE" -u "$MONGO_ORIGIN_USER" -p "$MONGO_ORIGIN_PASSWORD" -o mongodb_backup
 
 echo "Uploading backup"
-mongorestore --drop -h "$MONGO_DESTINATION_HOST" -d "$MONGO_DATABASE" -u "$MONGO_DESTINATION_USER" -p "$MONGO_DESTINATION_PASSWORD" "mongodb_backup/$MONGO_DATABASE"
+mongorestore --drop -h "$MONGO_DESTINATION_HOST" -d "$MONGO_DATABASE" -u "$MONGO_DESTINATION_USER" -p "$MONGO_DESTINATION_PASSWORD" "mongodb_backup/$MONGO_DATABASE" --authenticationDatabase "$MONGO_DESTINATION_AUTH_DB"
 
 echo "Removing local backup"
 rm -rf mongodb_backup
