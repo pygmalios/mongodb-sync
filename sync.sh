70dc33bfe6e0181422f6d6f92a17801733dfde01
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2018-05-09 14:34:59 +0200

    Optional destination auth db

--- sync.sh
+++ sync.sh
@@ -10,7 +10,11 @@ echo "Backing up $MONGO_DATABASE"
 mongodump -h "$MONGO_ORIGIN_HOST" -d "$MONGO_DATABASE" -u "$MONGO_ORIGIN_USER" -p "$MONGO_ORIGIN_PASSWORD" -o mongodb_backup
 
 echo "Uploading backup"
-mongorestore --drop -h "$MONGO_DESTINATION_HOST" -d "$MONGO_DATABASE" -u "$MONGO_DESTINATION_USER" -p "$MONGO_DESTINATION_PASSWORD" "mongodb_backup/$MONGO_DATABASE" --authenticationDatabase "$MONGO_DESTINATION_AUTH_DB"
+if [ ! -z ${MONGO_DESTINATION_AUTH_DB:-} ]; then
+  mongorestore --drop -h "$MONGO_DESTINATION_HOST" -d "$MONGO_DATABASE" -u "$MONGO_DESTINATION_USER" -p "$MONGO_DESTINATION_PASSWORD" "mongodb_backup/$MONGO_DATABASE" --authenticationDatabase "$MONGO_DESTINATION_AUTH_DB"
+else
+  mongorestore --drop -h "$MONGO_DESTINATION_HOST" -d "$MONGO_DATABASE" -u "$MONGO_DESTINATION_USER" -p "$MONGO_DESTINATION_PASSWORD" "mongodb_backup/$MONGO_DATABASE"
+fi
 
 echo "Removing local backup"
 rm -rf mongodb_backup
